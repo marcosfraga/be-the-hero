bf9af839b97b41e6618d832339c7e0e2ab6d1fcb
Marcos Fraga <marcosjsfraga@gmail.com>
2020-03-26 08:02:36 -0300

    Aula 2

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const routes = require('./routes');
 
 const app = express();
 
-app.use(cors);
+// app.use(cors);
 app.use(express.json()); // <- Converte corpo requisições HTTP para JSON
 app.use(routes);
 
